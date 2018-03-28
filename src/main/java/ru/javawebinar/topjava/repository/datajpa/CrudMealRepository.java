add96222c95aa98d92d86e46b28fd10e1bfeb658
iliapastushenko <ilya.pastushenko@bk.ru>
2018-03-28 19:34:57 +1000

    - DataJpaMealRepositoryImpl was implemented.

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,7 +1,33 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+
+    @Query("SELECT m FROM Meal m WHERE m.id=:id AND m.user.id=:userId")
+    Meal getByMealAndUserId(@Param("id") Integer id, @Param("userId") Integer userId);
+
+    @Query("SELECT m FROM Meal m WHERE m.user.id=:userId ORDER BY m.dateTime DESC")
+    List<Meal> getAllByUserIdSortedByDate(@Param("userId") Integer userId);
+
+    @Query("SELECT m FROM Meal m WHERE m.user.id=:userId AND m.dateTime BETWEEN :startDate AND :endDate ORDER BY m.dateTime DESC")
+    List<Meal> getAllByUserIdBetweenDatesSortedByDate(@Param("userId") Integer userId, @Param("startDate") LocalDateTime startDate,
+                                                      @Param("endDate") LocalDateTime endDate);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:userId")
+    int deleteByUserIdAndMealId(@Param("id") int id, @Param("userId") Integer userId);
+
 }
--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepositoryImpl.java
@@ -3,7 +3,6 @@ package ru.javawebinar.topjava.repository.datajpa;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
-import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
 import java.time.LocalDateTime;
@@ -13,44 +12,41 @@ import java.util.Objects;
 @Repository
 public class DataJpaMealRepositoryImpl implements MealRepository {
 
+
     @Autowired
-    private CrudMealRepository crudRepository;
+    private CrudMealRepository crudMealRepository;
 
     @Autowired
     private CrudUserRepository crudUserRepository;
 
+
+    @Override
+    public Meal get(int id, int userId) {
+        return crudMealRepository.getByMealAndUserId(id, userId);
+    }
+
     @Override
     public Meal save(Meal meal, int userId) {
         Objects.requireNonNull(meal);
-        if (meal.isNew()) {
-            meal.setUser(crudUserRepository.getOne(userId));
-            crudRepository.save(meal);
-        } else {
-            if (meal.getUser().getId() == userId) {
-                crudRepository.save(meal);
-            }
+        if (!meal.isNew() && get(meal.getId(), userId) == null) {
+            return null;
         }
-
-        return null;
+        meal.setUser(crudUserRepository.getOne(userId));
+        return crudMealRepository.save(meal);
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
-    }
-
-    @Override
-    public Meal get(int id, int userId) {
-        return null;
+        return crudMealRepository.deleteByUserIdAndMealId(id, userId) != 0;
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return crudMealRepository.getAllByUserIdSortedByDate(userId);
     }
 
     @Override
     public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return null;
+        return crudMealRepository.getAllByUserIdBetweenDatesSortedByDate(userId, startDate, endDate);
     }
 }
