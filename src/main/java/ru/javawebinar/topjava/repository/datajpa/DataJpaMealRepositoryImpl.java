3e8ba859c13618d09932f5c33d596430e06aa14a
iliapastushenko <ilya.pastushenko@bk.ru>
2018-03-28 18:17:53 +1000

    - tmp commit;

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepositoryImpl.java
@@ -3,10 +3,12 @@ package ru.javawebinar.topjava.repository.datajpa;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 @Repository
 public class DataJpaMealRepositoryImpl implements MealRepository {
@@ -14,8 +16,21 @@ public class DataJpaMealRepositoryImpl implements MealRepository {
     @Autowired
     private CrudMealRepository crudRepository;
 
+    @Autowired
+    private CrudUserRepository crudUserRepository;
+
     @Override
-    public Meal save(Meal Meal, int userId) {
+    public Meal save(Meal meal, int userId) {
+        Objects.requireNonNull(meal);
+        if (meal.isNew()) {
+            meal.setUser(crudUserRepository.getOne(userId));
+            crudRepository.save(meal);
+        } else {
+            if (meal.getUser().getId() == userId) {
+                crudRepository.save(meal);
+            }
+        }
+
         return null;
     }
 
